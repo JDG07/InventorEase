df95f1ad71c3168686bf723f10eeb849ff6bc84d
mashiroval <vincentnuilan@gmail.com>
2024-03-10 00:43:50 +0800

    test

--- app/src/main/java/com/example/inventorease_project/SalesEntry.java
+++ app/src/main/java/com/example/inventorease_project/SalesEntry.java
@@ -10,5 +10,7 @@ public class SalesEntry extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sales_entry);
+
+        //asadasdas
     }
 }
